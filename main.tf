1f264f7d68695d5a23627b12cfcba07cceba31ad
Jun Siang Cheah <git@jscheah.me>
2024-08-28 08:38:57 +0100

    fix: bad ifs

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "random_pet" "project" {
 
 resource "google_project" "project" {
   name            = var.name
-  project_id      = var.id ? var.id : random_pet.project.id
+  project_id      = var.id != null ? var.id : random_pet.project.id
   org_id          = var.org_id
   billing_account = var.billing_account
 }
@@ -15,7 +15,7 @@ resource "google_project_service" "gemini" {
 }
 
 resource "google_apikeys_key" "gemini" {
-  count = var.api_key_name ? 1 : 0
+  count = var.api_key_name != null ? 1 : 0
 
   project      = google_project.project.project_id
   name         = var.api_key_name
@@ -33,5 +33,5 @@ output "project_id" {
 }
 
 output "api_key" {
-  value = var.api_key_name ? google_apikeys_key.gemini[0].key_string : ""
+  value = var.api_key_name != null ? google_apikeys_key.gemini[0].key_string : ""
 }
